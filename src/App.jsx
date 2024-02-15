3de11a33884a15276318996ee536c659f9222963
JafetMo5601 <jafet.moraugalde15@gmail.com>
2024-02-15 14:30:57 -0600

    init project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
+
+function App() {
+  return (
+    <main>
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </main>
+  );
+}
+
+export default App;
