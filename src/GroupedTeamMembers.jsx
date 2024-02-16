0862082febfd975e6695255c99b48d69a7ec6678
JafetMo5601 <jafet.moraugalde15@gmail.com>
2024-02-16 16:12:48 -0600

    Create the component groupedTeamMembers

---
+++ src/GroupedTeamMembers.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+
+const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
+    
+    return (
+        <main className="container">
+            <div className="row justify-content-center mt-3 mb-4">
+                <div className="col-8">
+                    <h1>Grouped Team Member</h1>
+                </div>
+            </div>
+        </main>
+    )
+}
+export default GroupedTeamMembers;
